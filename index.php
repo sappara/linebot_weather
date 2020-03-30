f59d6091c1c6bd812d23f1d1ea6ba3cd1a3e926e
saharei <saharai.co@gmail.com>
2020-03-30 09:48:31 +0900

    インデント再調整(前コミットでも調整した)

--- index.php
+++ index.php
@@ -47,100 +47,100 @@ foreach ($events as $event) {
   }
   // LocationMessageクラスのインスタンスの場合
   else if ($event instanceof \LINE\LINEBot\Event\MessageEvent\LocationMessage) {
-    // LocationMessageの内容を返す
-    // replyTextMessage($bot, $event->getReplyToken(), $event->getAddress().'['.$event->getLatitude().','.$event->getLongitude().']');
-    // continue;
-  // Google APIにアクセスし緯度経度から住所を取得
-  $jsonString = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?language=ja&latlng=' . $event->getLatitude() . ',' . $event->getLongitude());
-  // 文字列を連想配列に変換
-  $json = json_decode($jsonString, true);
-  // 住所情報のみを取り出し
-  $addressComponentArray = $json['results'][0]['address_components'];
-  // 要素をループで処理
-  foreach($addressComponentArray as $addressComponent) {
-    // 県名を取得
-    if(in_array('administrative_area_level_1', $addressComponent['types'])) {
-      $prefName = $addressComponent['long_name'];
-      break;
-    }
-  }
-  // 東京と大阪の場合他県と内容が違うので特別な処理
-  if($prefName == '東京都') {
-    $location = '東京';
-  } else if($prefName == '大阪府') {
-    $location = '大阪';
-  // それ以外なら
-  } else {
+      // LocationMessageの内容を返す
+      // replyTextMessage($bot, $event->getReplyToken(), $event->getAddress().'['.$event->getLatitude().','.$event->getLongitude().']');
+      // continue;
+    // Google APIにアクセスし緯度経度から住所を取得
+    $jsonString = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?language=ja&latlng=' . $event->getLatitude() . ',' . $event->getLongitude());
+    // 文字列を連想配列に変換
+    $json = json_decode($jsonString, true);
+    // 住所情報のみを取り出し
+    $addressComponentArray = $json['results'][0]['address_components'];
     // 要素をループで処理
     foreach($addressComponentArray as $addressComponent) {
-      // 市名を取得
-      if(in_array('locality', $addressComponent['types']) && !in_array('ward', $addressComponent['types'])) {
-        $location = $addressComponent['long_name'];
+      // 県名を取得
+      if(in_array('administrative_area_level_1', $addressComponent['types'])) {
+        $prefName = $addressComponent['long_name'];
         break;
       }
     }
-  }
-}
-
-// 住所ID用変数
-$locationId;
-// XMLファイルをパースするクラス
-$client = new Goutte\Client();
-// XMLファイルを取得
-$crawler = $client->request('GET', 'http://weather.livedoor.com/forecast/rss/primary_area.xml');
-// 市名のみを抽出しユーザーが入力した市名と比較
-foreach ($crawler->filter('channel ldWeather|source pref city') as $city) {
-  // 一致すれば住所IDを取得し処理を抜ける
-  if($city->getAttribute('title') == $location || $city->getAttribute('title') . "市" == $location) {
-    $locationId = $city->getAttribute('id');
-    break;
-  }
-}
-// 一致するものが無ければ
-if(empty($locationId)) {
-  // 位置情報が送られた時は県名を取得済みなのでそれを代入
-  if ($event instanceof \LINE\LINEBot\Event\MessageEvent\LocationMessage) {
-    $location = $prefName;
-  }
-  // 候補の配列
-  $suggestArray = array();
-  // 県名を抽出しユーザーが入力した県名と比較
-  foreach ($crawler->filter('channel ldWeather|source pref') as $pref) {
-    // 一致すれば
-    if(strpos($pref->getAttribute('title'), $location) !== false) {
-      // その県に属する市を配列に追加
-      foreach($pref->childNodes as $child) {
-        if($child instanceof DOMElement && $child->nodeName == 'city') {
-          array_push($suggestArray, $child->getAttribute('title'));
+    // 東京と大阪の場合他県と内容が違うので特別な処理
+    if($prefName == '東京都') {
+      $location = '東京';
+    } else if($prefName == '大阪府') {
+      $location = '大阪';
+    // それ以外なら
+    } else {
+      // 要素をループで処理
+      foreach($addressComponentArray as $addressComponent) {
+        // 市名を取得
+        if(in_array('locality', $addressComponent['types']) && !in_array('ward', $addressComponent['types'])) {
+          $location = $addressComponent['long_name'];
+          break;
         }
       }
+    }
+  }
+
+  // 住所ID用変数
+  $locationId;
+  // XMLファイルをパースするクラス
+  $client = new Goutte\Client();
+  // XMLファイルを取得
+  $crawler = $client->request('GET', 'http://weather.livedoor.com/forecast/rss/primary_area.xml');
+  // 市名のみを抽出しユーザーが入力した市名と比較
+  foreach ($crawler->filter('channel ldWeather|source pref city') as $city) {
+    // 一致すれば住所IDを取得し処理を抜ける
+    if($city->getAttribute('title') == $location || $city->getAttribute('title') . "市" == $location) {
+      $locationId = $city->getAttribute('id');
       break;
     }
   }
-// 候補が存在する場合
-if(count($suggestArray) > 0) {
-  // アクションの配列
-  $actionArray = array();
-  //候補を全てアクションにして追加
-  foreach($suggestArray as $city) {
-    array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder ($city, $city));
+  // 一致するものが無ければ
+  if(empty($locationId)) {
+    // 位置情報が送られた時は県名を取得済みなのでそれを代入
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\LocationMessage) {
+      $location = $prefName;
+    }
+    // 候補の配列
+    $suggestArray = array();
+    // 県名を抽出しユーザーが入力した県名と比較
+    foreach ($crawler->filter('channel ldWeather|source pref') as $pref) {
+      // 一致すれば
+      if(strpos($pref->getAttribute('title'), $location) !== false) {
+        // その県に属する市を配列に追加
+        foreach($pref->childNodes as $child) {
+          if($child instanceof DOMElement && $child->nodeName == 'city') {
+            array_push($suggestArray, $child->getAttribute('title'));
+          }
+        }
+        break;
+      }
+    }
+    // 候補が存在する場合
+    if(count($suggestArray) > 0) {
+      // アクションの配列
+      $actionArray = array();
+      //候補を全てアクションにして追加
+      foreach($suggestArray as $city) {
+        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder ($city, $city));
+      }
+      // Buttonsテンプレートを返信
+      $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
+        '見つかりませんでした。',
+        new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder ('見つかりませんでした。', 'もしかして？', null, $actionArray));
+        $bot->replyMessage($event->getReplyToken(), $builder
+      );
+    }
+    // 候補が存在しない場合
+    else {
+      // 正しい入力方法を返信
+      replyTextMessage($bot, $event->getReplyToken(), '入力された地名が見つかりませんでした。市を入力してください。');
+    }
+    // 以降の処理はスキップ
+    continue;
   }
-  // Buttonsテンプレートを返信
-  $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
-    '見つかりませんでした。',
-    new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder ('見つかりませんでした。', 'もしかして？', null, $actionArray));
-    $bot->replyMessage($event->getReplyToken(), $builder
-  );
-}
-// 候補が存在しない場合
-else {
-  // 正しい入力方法を返信
-  replyTextMessage($bot, $event->getReplyToken(), '入力された地名が見つかりませんでした。市を入力してください。');
-}
-// 以降の処理はスキップ
-continue;
-}
-replyTextMessage($bot, $event->getReplyToken(), $location.'の住所IDは'.$locationId."です。");
+  replyTextMessage($bot, $event->getReplyToken(), $location.'の住所IDは'.$locationId."です。");
 }
 
 // テキストを返信。引数はLINEBot、返信先、テキスト
